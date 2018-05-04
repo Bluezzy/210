69b245ef0e31427908517631d72e291d01b123b2
Marc'Andria Battesti <marcandriabattesti@gmail.com>
2018-05-04 19:38:44 +0200

    Practice : Logic and Flow Control

--- practice/rot13.js
+++ practice/rot13.js
@@ -1,41 +1,47 @@
 function isInAlphabet(character) {
-  character = String(character);
-  if (character.length != 1) {
-    return false;
-  }
-
   var asciiNumeric = character.toLowerCase().charCodeAt(); // 'A' to 'Z' are valid after being lowercased.
   return (asciiNumeric >= 97 && asciiNumeric <= 122); 
 }
 
-function isInlastHalfAlphabet(character) { // ('n' to 'z') or ('N' to 'Z')
-  var asciiNumeric = character.toLowerCase().charCodeAt();
-  return (asciiNumeric <= 122 && asciiNumeric >= 110);
+function isInlastHalfAlphabet(charCode) { // ('n' to 'z') or ('N' to 'Z')
+  return ((charCode <= 122 && charCode >= 110) || (charCode <= 90 && charCode >= 78));
 }
 
-function cipher(character) {
-  var charCode = character.charCodeAt();
-  var cipheredCharCode = charCode;
-  var cipheredCharacter;
+function goDownTheAlphabet(charCode) {
+  return (charCode - 13);
+}
 
-  if (isInAlphabet(character)) { 
-    if (isInlastHalfAlphabet(character)) {
-      cipheredCharCode -= 13; // we go down half of the alphabet
-    } else { // all remaining characters
-      cipheredCharCode += 13; // we go up half of the alphabet
-    }
+function goUpTheAlphabet(charCode) {
+  return (charCode + 13);
+}
+
+function cipher(charCode) {
+  if (isInlastHalfAlphabet(charCode)) {
+    charCode = goDownTheAlphabet(charCode);
+  } else {
+    charCode = goUpTheAlphabet(charCode);
   }
+  return charCode;
+}
 
-  cipheredCharacter = String.fromCharCode(cipheredCharCode); // convert our modified number back to a caracter.
-  return cipheredCharacter;
+function mutate(character) {
+  var charCode = character.charCodeAt();
+  var cipheredCharCode = cipher(charCode);
+  var mutatedCharacter = String.fromCharCode(cipheredCharCode);
+  return mutatedCharacter;
 }
 
 function rot13(string) {
+  string = String(string);
   var i;
   var newStr = '';
 
   for (i = 0; i < string.length; i++) {
-    newStr += cipher(string[i])
+    if (isInAlphabet(string[i])) {
+      newStr += mutate(string[i]);
+    } else {
+      newStr += string[i];
+    }
   }
 
   return newStr;
