d023d795b0c97a1d0b7f518c8678530156ef02e1
Marc Battesti <marcandriabattesti@gmail.com>
2018-05-11 16:53:47 +0200

    Objects

--- objects/practice.js
+++ objects/practice.js
@@ -44,3 +44,37 @@ function greetings(name, status) {
 	console.log('Hello, ' + nameOutput + '! We\'re proud to have a ' + statusOutput + ' here !')
 }
 
+function repeatedCharacters(string) {
+	var str = string.toLowerCase();
+	var characters = str.split('');
+	var frequency = {};
+	var i;
+
+	for (i = 0; i < str.length; i++) {
+		if (frequency[str[i]] !== undefined) {
+			frequency[str[i]]++;
+		} else {
+			frequency[str[i]] = 1;
+		}
+	}
+
+
+	for (character in frequency) {
+		if (frequency[character] === 1) {
+			delete frequency[character];
+		}
+  }
+	return frequency;
+}
+
+// lowercase the string.
+// split characters => array of characters
+// low
+// initialize a frequency object => object { character : count }
+// loop through each character of the array :
+//  - if object[character] exists, add 1
+//  = else, set object[character] =.
+//
+// for key in object
+// if object[key] === 1, delete object[key]
+// return object
